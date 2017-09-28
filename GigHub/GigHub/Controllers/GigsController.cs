8dafec3092de78c9d16348f4f3ed13f8ae7e50c8
Vladislav Volkov <vladislaw.volkov@gmail.com>
2017-09-28 08:55:51 +0000

    Implement "My Upcoming Gigs" case. Imported from Changeset 25

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -49,7 +49,7 @@ namespace GigHub.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
 
         [Authorize]
@@ -76,7 +76,7 @@ namespace GigHub.Controllers
         [Authorize]
         public ActionResult Mine()
         {
-            var userId = User.Identity.AuthenticationType;
+            var userId = User.Identity.GetUserId();
             var gigs = _context.Gigs
                 .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
                 .Include(g => g.Genre)
